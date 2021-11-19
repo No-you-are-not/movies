e991dae60e1aabb508703bc3f32b30cd9c0042b1
Polina <polina.doctorwho@gmail.com>
2021-11-19 16:31:44 +0200

    fixed bug

--- movies/src/components/Header.js
+++ movies/src/components/Header.js
@@ -9,7 +9,7 @@ export default function Header(){
             <div className={'title'}>✧･ﾟ: *✧･ﾟ:* Best movies storage *:･ﾟ✧*:･ﾟ✧</div>
             <div className={'user-info'}>
                 <img alt={'user logo'} className={'user-img'} src={"https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-alt-512.png"}/>
-                <div className={'user-name'}>{user[0]}</div>
+                <div className={'user-name'}>{user[0] ? user[0] : 'user'}</div>
             </div>
         </div>
     )
